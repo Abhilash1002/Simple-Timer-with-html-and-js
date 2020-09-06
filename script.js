527b55b935ea5379dabaf98ae90fde8d39465b83
Abhilash Gudisena <42652307+Abhilash1002@users.noreply.github.com>
2020-09-07 03:49:02 +0530

    Update script.js
    changed background color to linear gradient colors

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const button = document.querySelector('.clicker');
-const body = document.querySelector('body');
+const outerbox = document.querySelector('.outerbox');
+// const body = document.querySelector('body');
 console.log(button.innerHTML)
 
+function setRandomColor(element){
+    element.background = getRandomColor();
+}
 function getRandomColor(){
     let a = Math.floor(Math.random() * 256);
     let b = Math.floor(Math.random() * 256);
     let c = Math.floor(Math.random() * 256);
-    return 'rgb(' + a + ',' + b + ',' + c + ')';
+    let d = Math.floor(Math.random() * 256);
+    let e = Math.floor(Math.random() * 256);
+    let f = Math.floor(Math.random() * 256);
+    let left = 'rgb(' + a + ',' + b + ',' + c + ')';
+    let right = 'rgb(' + d + ',' + e + ',' + f + ')';
+
+    return "linear-gradient(to left," + left + "," + right + ")";
 }
 function countdown(seconds){
     let output = document.querySelector('.seconds');
@@ -18,12 +28,17 @@ function countdown(seconds){
         for(let i=seconds;i>=0;i--){
             if(i == 0){
                 setTimeout(function(){ 
-                                       output.innerHTML = "BOOM!!";
-                                       body.bgColor = getRandomColor();
-                                    //    output.setAttribute = getRandomColor();
+                    output.innerHTML = "BOOM!!";
+                    setRandomColor(document.body.style);
+                    // setRandomColor(outerbox.style);
                  },(seconds - i)*1000); 
-            }else{
-            setTimeout(function(){ output.innerHTML = "" + i +" ";},(seconds - i)*1000);
+            }
+            else{
+                setTimeout(function(){ 
+                    output.innerHTML = "" + i +" ";
+                    setRandomColor(document.body.style);
+                    // setRandomColor(outerbox.style);
+                },(seconds - i)*1000);
             }
         }
         console.log("yo");
